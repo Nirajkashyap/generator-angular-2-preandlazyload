f9ca6e46f382b08959cf47ffcc5eb6215c254f42
nirajk <niraj38re@gmail.com>
2017-03-02 21:54:53 +0530

    header cmp added

---
+++ generators/app/templates/src/app/components/header/header.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'header'
+  selector: 'header',  // <header></header>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  
+  // Our list of styles in our component. We may add more to compose many styles together
+  // styleUrls: [ './header.component.css' ],
+  styles: [
+    require('./header.component.css')    
+  ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  templateUrl: './header.component.html'
+})
+export class headerComponent implements OnInit {
+  // Set our default values
+  public localState = { value: '' };
+  public headervalue = "header";
+  // TypeScript public modifiers
+  constructor(
+    public appState: AppState
+    
+  ) {}
+
+  public ngOnInit() {
+    console.log('hello `header` component');
+    
+  }
+
+  public submitState(value: string) {
+    console.log('submited value from fn argument', value);
+    console.log(this.headervalue);
+    
+  }
+}

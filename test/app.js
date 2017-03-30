fc96ef2b0dad87377e91d45a1b4f6e5ceafd638c
nirajk <niraj38re@gmail.com>
2017-03-30 21:25:36 +0530

    generator testing and publish file added

--- test/app.js
+++ test/app.js
@@ -6,7 +6,7 @@ var helpers = require('yeoman-test');
 describe('generator-angular-2-preandlazyload:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({ someAnswer: true })
       .toPromise();
   });
 

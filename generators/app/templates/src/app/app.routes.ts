fcfaec2ec2f3f4c1f3eb58eebca20a1d3def839b
nirajk <niraj38re@gmail.com>
2017-03-30 21:58:26 +0530

    re-factoring app routes code base

--- generators/app/templates/src/app/app.routes.ts
+++ generators/app/templates/src/app/app.routes.ts
@@ -1,40 +1,30 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { CanDeactivateGuard }       from './can-deactivate-guard.service';
-import { AuthGuard }                from './auth-guard.service';
+import { CanDeactivateGuard } from './can-deactivate-guard.service';
+import { AuthGuard } from './auth-guard.service';
 import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 
-import { HomeComponent } from './home/home.component';
-
 // Observable Version
-import { Injectable }              from '@angular/core';
-import { Http, Response }          from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
-
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+// base model for route object
 import { Routeobj } from './app.routeobj';
+
+import { HomeComponent } from './pages/home';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent    
-  }, 
-  {
-    path: 'preload',
-    loadChildren: 'app/admin/admin.module#AdminModule',
-    data: { preload: false }    
-  },
-  {
-    path: 'lazyloadwithcanload',
-    loadChildren: 'app/admin/admin.module#AdminModule',
-    canLoad: [AuthGuard]
+    component: HomeComponent
   }
-  
+
 ];
 
 @NgModule({
@@ -46,7 +36,7 @@ const appRoutes: Routes = [
   ],
   declarations: [
     HomeComponent
-  ], 
+  ],
   exports: [
     RouterModule
   ],
@@ -57,38 +47,34 @@ const appRoutes: Routes = [
 })
 
 @Injectable()
-export class AppRoutingModule { 
+export class AppRoutingModule {
 
   public value = 'Angular 2';
 
   constructor(
     public http: Http
   ) {
-    this.getHeroes();
+    this.getRoutes();
   }
 
-  // public getData() {
-  //   console.log('Title#getData(): Get Data');
-  //   return this.http.get('/app.routes.json')
-  //   .map(this.extractData)
-  //   //.map(res => res.json());
-  //   // return {
-  //   //   value: 'AngularClass'
-  //   // };
-  // }
 
-  getHeroes (): Observable<Routeobj[]> {
+  public getRoutes(): Observable<Routeobj[]> {
     console.log("res......")
     return this.http.get('/app.routes.json')
-                    .map(this.extractData);
-                    //.catch(this.handleError);
+      .map(this.extractData);
+      .catch(this.handleError);
   }
 
   private extractData(res: Response) {
-    console.log("res...receivedls");
+    console.log("receving data");
     console.log(res);
     let body = res.json();
-    return body.data || { };
+    return body.data || {};
+  }
+
+  private handleError(error) {
+    // In a real world app, we might use a remote logging infrastructure
+
   }
 
 
